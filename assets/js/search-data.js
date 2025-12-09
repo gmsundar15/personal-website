4f750490b0d891a7e06fef8eab96f8a3b6fedbbd
neuroconvergent <76529072+neuroconvergent@users.noreply.github.com>
2025-12-09 07:14:16 +0000

    Deploying to gh-pages from @ neuroconvergent/personal-website@2be8e2d48c9d7f61ceb45c3cdf7a37c3580613d3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -79,6 +79,13 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_kMC_FEA/";
             },},{
+        id: 'social-cv',
+        title: 'CV',
+        section: 'Socials',
+        handler: () => {
+          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+        },
+      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
